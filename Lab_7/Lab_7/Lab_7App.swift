f8c360f382f3a0808cd45957a0d61bef6983b65f
JarneAdam <jarne.adam3@gmail.com>
2025-12-03 09:21:00 +0100

    Added given files to project

---
+++ Lab_7/Lab_7/Lab_7App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_7App.swift
+//  Lab_7
+//
+//  Created by Jarne Adam on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Lab_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
